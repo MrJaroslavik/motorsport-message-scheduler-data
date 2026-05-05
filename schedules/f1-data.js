3e2901edbcf0240f63187b06168141c8a0e4e5e4
MrJaroslavik <MrJaroslavik@gmail.com>
2026-05-05 14:22:39 +0200

    Update f1-data.js

--- schedules/f1-data.js
+++ schedules/f1-data.js
@@ -3,56 +3,21 @@
 
 export const F1_WEEKENDS = [
   {
-    slug: "f1_china_2026",
-    name: "Chinese",
-    sessions: [
-      { slug: "fp1", label: "Practice 1", datetime: "2026-03-13 04:30" },
-      { slug: "sprint_qualifying", label: "Sprint Qualifying", datetime: "2026-03-13 08:30" },
-      { slug: "sprint", label: "Sprint", datetime: "2026-03-14 04:00" },
-      { slug: "qualifying", label: "Qualifying", datetime: "2026-03-14 08:00" },
-      { slug: "race", label: "Race", datetime: "2026-03-15 08:00" },
-    ],
-  },
-
-  {
-    slug: "f1_japan_2026",
-    name: "Japanese",
-    sessions: [
-      { slug: "fp1", label: "Practice 1", datetime: "2026-03-27 03:30" },
-      { slug: "fp2", label: "Practice 2", datetime: "2026-03-27 07:00" },
-      { slug: "fp3", label: "Practice 3", datetime: "2026-03-28 03:30" },
-      { slug: "qualifying", label: "Qualifying", datetime: "2026-03-28 07:00" },
-      { slug: "race", label: "Race", datetime: "2026-03-29 07:00" },
-    ],
-  },
-  
-  {
-    slug: "f1_miami_2026",
-    name: "Miami",
-    sessions: [
-      { slug: "fp1", label: "Practice 1", datetime: "2026-05-01 18:30" },
-      { slug: "sprint_qualifying", label: "Sprint Qualifying", datetime: "2026-05-01 22:30" },
-      { slug: "sprint", label: "Sprint", datetime: "2026-05-02 18:00" },
-      { slug: "qualifying", label: "Qualifying", datetime: "2026-05-02 22:00" },
-      { slug: "race", label: "Race", datetime: "2026-05-03 19:00" },
-    ],
-  },
-
-  {
     slug: "f1_canada_2026",
     name: "Canadian",
+    link: "https://cal.f1.com/f/28Tbsd/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-05-22 18:30" },
       { slug: "sprint_qualifying", label: "Sprint Qualifying", datetime: "2026-05-22 22:30" },
-      { slug: "sprint", label: "Sprint", datetime: "2026-05-23 18:00" },
+      { slug: "sprint", label: "Sprint Race", datetime: "2026-05-23 18:00" },
       { slug: "qualifying", label: "Qualifying", datetime: "2026-05-23 22:00" },
       { slug: "race", label: "Race", datetime: "2026-05-24 22:00" },
     ],
   },
-
   {
     slug: "f1_monaco_2026",
     name: "Monaco",
+    link: "https://cal.f1.com/f/28TchX/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-06-05 13:30" },
       { slug: "fp2", label: "Practice 2", datetime: "2026-06-05 17:00" },
@@ -61,10 +26,10 @@ export const F1_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-06-07 15:00" },
     ],
   },
-
   {
     slug: "f1_barcelona_2026",
     name: "Barcelona-Catalunya",
+    link: "https://cal.f1.com/f/28Tf5P/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-06-12 13:30" },
       { slug: "fp2", label: "Practice 2", datetime: "2026-06-12 17:00" },
@@ -73,10 +38,10 @@ export const F1_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-06-14 15:00" },
     ],
   },
-
   {
     slug: "f1_austria_2026",
     name: "Austrian",
+    link: "https://cal.f1.com/f/28T7dK/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-06-26 13:30" },
       { slug: "fp2", label: "Practice 2", datetime: "2026-06-26 17:00" },
@@ -85,22 +50,22 @@ export const F1_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-06-28 15:00" },
     ],
   },
-
   {
     slug: "f1_britain_2026",
     name: "British",
+    link: "https://cal.f1.com/f/28T8pv/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-07-03 13:30" },
       { slug: "sprint_qualifying", label: "Sprint Qualifying", datetime: "2026-07-03 17:30" },
-      { slug: "sprint", label: "Sprint", datetime: "2026-07-04 13:00" },
+      { slug: "sprint", label: "Sprint Race", datetime: "2026-07-04 13:00" },
       { slug: "qualifying", label: "Qualifying", datetime: "2026-07-04 17:00" },
       { slug: "race", label: "Race", datetime: "2026-07-05 16:00" },
     ],
   },
-
   {
     slug: "f1_belgium_2026",
     name: "Belgian",
+    link: "https://cal.f1.com/f/28T9tn/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-07-17 13:30" },
       { slug: "fp2", label: "Practice 2", datetime: "2026-07-17 17:00" },
@@ -109,10 +74,10 @@ export const F1_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-07-19 15:00" },
     ],
   },
-
   {
     slug: "f1_hungary_2026",
     name: "Hungarian",
+    link: "https://cal.f1.com/f/28T4kW/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-07-24 13:30" },
       { slug: "fp2", label: "Practice 2", datetime: "2026-07-24 17:00" },
@@ -121,22 +86,22 @@ export const F1_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-07-26 15:00" },
     ],
   },
-
   {
     slug: "f1_netherlands_2026",
     name: "Dutch",
+    link: "https://cal.f1.com/f/28T5GL/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-08-21 12:30" },
       { slug: "sprint_qualifying", label: "Sprint Qualifying", datetime: "2026-08-21 16:30" },
-      { slug: "sprint", label: "Sprint", datetime: "2026-08-22 12:00" },
+      { slug: "sprint", label: "Sprint Race", datetime: "2026-08-22 12:00" },
       { slug: "qualifying", label: "Qualifying", datetime: "2026-08-22 16:00" },
       { slug: "race", label: "Race", datetime: "2026-08-23 15:00" },
     ],
   },
-
   {
     slug: "f1_italy_2026",
     name: "Italian",
+    link: "https://cal.f1.com/f/28TfKX/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-09-04 12:30" },
       { slug: "fp2", label: "Practice 2", datetime: "2026-09-04 16:00" },
@@ -145,10 +110,10 @@ export const F1_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-09-06 15:00" },
     ],
   },
-
   {
     slug: "f1_spain_2026",
     name: "Spanish",
+    link: "https://cal.f1.com/f/28TbzQ/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-09-11 13:30" },
       { slug: "fp2", label: "Practice 2", datetime: "2026-09-11 17:00" },
@@ -157,10 +122,10 @@ export const F1_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-09-13 15:00" },
     ],
   },
-
   {
     slug: "f1_azerbaijan_2026",
     name: "Azerbaijan",
+    link: "https://cal.f1.com/f/28TbHX/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-09-24 10:30" },
       { slug: "fp2", label: "Practice 2", datetime: "2026-09-24 14:00" },
@@ -169,22 +134,22 @@ export const F1_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-09-26 13:00" },
     ],
   },
-
-{
-  slug: "f1_singapore_2026",
-  name: "Singapore",
-  sessions: [
-    { slug: "fp1", label: "Practice 1", datetime: "2026-10-09 10:30" },
-    { slug: "sprint_qualifying", label: "Sprint Qualifying", datetime: "2026-10-09 14:30" },
-    { slug: "sprint", label: "Sprint", datetime: "2026-10-10 11:00" },
-    { slug: "qualifying", label: "Qualifying", datetime: "2026-10-10 15:00" },
-    { slug: "race", label: "Race", datetime: "2026-10-11 14:00" },
-  ],
-},
-
+  {
+    slug: "f1_singapore_2026",
+    name: "Singapore",
+    link: "https://cal.f1.com/f/28TcXN/3rT3K",
+    sessions: [
+      { slug: "fp1", label: "Practice 1", datetime: "2026-10-09 10:30" },
+      { slug: "sprint_qualifying", label: "Sprint Qualifying", datetime: "2026-10-09 14:30" },
+      { slug: "sprint", label: "Sprint Race", datetime: "2026-10-10 11:00" },
+      { slug: "qualifying", label: "Qualifying", datetime: "2026-10-10 15:00" },
+      { slug: "race", label: "Race", datetime: "2026-10-11 14:00" },
+    ],
+  },
   {
     slug: "f1_usa_2026",
     name: "United States",
+    link: "https://cal.f1.com/f/28TdvL/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-10-23 19:30" },
       { slug: "fp2", label: "Practice 2", datetime: "2026-10-23 23:00" },
@@ -193,10 +158,10 @@ export const F1_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-10-25 21:00" },
     ],
   },
-
   {
     slug: "f1_mexico_2026",
     name: "Mexican",
+    link: "https://cal.f1.com/f/28TgXC/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-10-30 19:30" },
       { slug: "fp2", label: "Practice 2", datetime: "2026-10-30 23:00" },
@@ -205,10 +170,10 @@ export const F1_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-11-01 21:00" },
     ],
   },
-
   {
     slug: "f1_brazil_2026",
     name: "Sao Paulo",
+    link: "https://cal.f1.com/f/28ThwD/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-11-06 16:30" },
       { slug: "fp2", label: "Practice 2", datetime: "2026-11-06 20:00" },
@@ -217,10 +182,10 @@ export const F1_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-11-08 18:00" },
     ],
   },
-
   {
     slug: "f1_las_vegas_2026",
     name: "Las Vegas",
+    link: "https://cal.f1.com/f/28Tj1t/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-11-20 01:30" },
       { slug: "fp2", label: "Practice 2", datetime: "2026-11-20 05:00" },
@@ -229,10 +194,10 @@ export const F1_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-11-22 05:00" },
     ],
   },
-
   {
     slug: "f1_qatar_2026",
     name: "Qatar",
+    link: "https://cal.f1.com/f/28T4GY/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-11-27 14:30" },
       { slug: "fp2", label: "Practice 2", datetime: "2026-11-27 18:00" },
@@ -241,10 +206,10 @@ export const F1_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-11-29 17:00" },
     ],
   },
-
   {
     slug: "f1_abu_dhabi_2026",
     name: "Abu Dhabi",
+    link: "https://cal.f1.com/f/28T6bP/3rT3K",
     sessions: [
       { slug: "fp1", label: "Practice 1", datetime: "2026-12-04 10:30" },
       { slug: "fp2", label: "Practice 2", datetime: "2026-12-04 14:00" },
