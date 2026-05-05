730e0661e4d2db12e9f5dba473adab4f1ce35d9c
MrJaroslavik <MrJaroslavik@gmail.com>
2026-05-05 15:01:15 +0200

    Update formulae data with new links and remove entries
    Removed several weekend entries and added links for Monaco, Sanya, Shanghai, Tokyo, and London events.

--- schedules/formulae-data.js
+++ schedules/formulae-data.js
@@ -4,40 +4,9 @@
 export const FORMULAE_WEEKENDS = [
 
   {
-    slug: "fe_madrid_2026",
-    name: "Madrid",
-    sessions: [
-      { slug: "fp1", label: "Free Practice 1", datetime: "2026-03-20 15:30" },
-      { slug: "fp2", label: "Free Practice 2", datetime: "2026-03-21 09:30" },
-      { slug: "qualifying", label: "Qualifying", datetime: "2026-03-21 11:40" },
-      { slug: "race", label: "Race", datetime: "2026-03-21 16:05" },
-    ],
-  },
-
-  {
-    slug: "fe_berlin1_2026",
-    name: "Berlin I",
-    sessions: [
-      { slug: "fp1", label: "Free Practice 1", datetime: "2026-05-01 16:00" },
-      { slug: "fp2", label: "Free Practice 2", datetime: "2026-05-02 09:30" },
-      { slug: "qualifying", label: "Qualifying", datetime: "2026-05-02 11:40" },
-      { slug: "race", label: "Race", datetime: "2026-05-02 16:05" },
-    ],
-  },
-
-  {
-    slug: "fe_berlin2_2026",
-    name: "Berlin II",
-    sessions: [
-      { slug: "fp3", label: "Free Practice 3", datetime: "2026-05-03 09:30" },
-      { slug: "qualifying", label: "Qualifying", datetime: "2026-05-03 11:40" },
-      { slug: "race", label: "Race", datetime: "2026-05-03 16:05" },
-    ],
-  },
-
-  {
     slug: "fe_monaco1_2026",
     name: "Monaco I",
+    link: "https://www.fiaformulae.com/en/calendar/2025-26/r9-monaco",
     sessions: [
       { slug: "fp1", label: "Free Practice 1", datetime: "2026-05-16 07:30" },
       { slug: "fp2", label: "Free Practice 2", datetime: "2026-05-16 09:10" },
@@ -45,20 +14,20 @@ export const FORMULAE_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-05-16 15:05" },
     ],
   },
-
   {
     slug: "fe_monaco2_2026",
     name: "Monaco II",
+    link: "https://www.fiaformulae.com/en/calendar/2025-26/r10-monaco",
     sessions: [
       { slug: "fp3", label: "Free Practice 3", datetime: "2026-05-17 08:30" },
       { slug: "qualifying", label: "Qualifying", datetime: "2026-05-17 10:40" },
       { slug: "race", label: "Race", datetime: "2026-05-17 15:05" },
     ],
   },
-
   {
     slug: "fe_sanya_2026",
     name: "Sanya",
+    link: "https://www.fiaformulae.com/en/calendar/2025-26/r11-sanya",
     sessions: [
       { slug: "fp1", label: "Free Practice 1", datetime: "2026-06-19 10:30" },
       { slug: "fp2", label: "Free Practice 2", datetime: "2026-06-20 02:30" },
@@ -66,10 +35,10 @@ export const FORMULAE_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-06-20 09:05" },
     ],
   },
-
   {
     slug: "fe_shanghai1_2026",
     name: "Shanghai I",
+    link: "https://www.fiaformulae.com/en/calendar/2025-26/r12-shanghai",
     sessions: [
       { slug: "fp1", label: "Free Practice 1", datetime: "2026-07-03 10:00" },
       { slug: "fp2", label: "Free Practice 2", datetime: "2026-07-04 02:30" },
@@ -77,20 +46,20 @@ export const FORMULAE_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-07-04 09:05" },
     ],
   },
-
   {
     slug: "fe_shanghai2_2026",
     name: "Shanghai II",
+    link: "https://www.fiaformulae.com/en/calendar/2025-26/r13-shanghai",
     sessions: [
       { slug: "fp3", label: "Free Practice 3", datetime: "2026-07-05 02:30" },
       { slug: "qualifying", label: "Qualifying", datetime: "2026-07-05 04:40" },
       { slug: "race", label: "Race", datetime: "2026-07-05 09:05" },
     ],
   },
-
   {
     slug: "fe_tokyo1_2026",
     name: "Tokyo I",
+    link: "https://www.fiaformulae.com/en/calendar/2025-26/r14-tokyo",
     sessions: [
       { slug: "fp1", label: "Free Practice 1", datetime: "2026-07-24 12:00" },
       { slug: "fp2", label: "Free Practice 2", datetime: "2026-07-25 06:30" },
@@ -98,20 +67,20 @@ export const FORMULAE_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-07-25 13:05" },
     ],
   },
-
   {
     slug: "fe_tokyo2_2026",
     name: "Tokyo II",
+    link: "https://www.fiaformulae.com/en/calendar/2025-26/r15-tokyo",
     sessions: [
       { slug: "fp3", label: "Free Practice 3", datetime: "2026-07-26 06:30" },
       { slug: "qualifying", label: "Qualifying", datetime: "2026-07-26 08:40" },
       { slug: "race", label: "Race", datetime: "2026-07-26 13:05" },
     ],
   },
-
   {
     slug: "fe_london1_2026",
     name: "London I",
+    link: "https://www.fiaformulae.com/en/calendar/2025-26/r16-london",
     sessions: [
       { slug: "fp1", label: "Free Practice 1", datetime: "2026-08-14 17:00" },
       { slug: "fp2", label: "Free Practice 2", datetime: "2026-08-15 09:30" },
@@ -119,15 +88,14 @@ export const FORMULAE_WEEKENDS = [
       { slug: "race", label: "Race", datetime: "2026-08-15 16:05" },
     ],
   },
-
   {
     slug: "fe_london2_2026",
     name: "London II",
+    link: "https://www.fiaformulae.com/en/calendar/2025-26/r17-london",
     sessions: [
       { slug: "fp3", label: "Free Practice 3", datetime: "2026-08-16 09:30" },
       { slug: "qualifying", label: "Qualifying", datetime: "2026-08-16 11:40" },
       { slug: "race", label: "Race", datetime: "2026-08-16 16:05" },
     ],
   },
-
 ];
