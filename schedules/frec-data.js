d2b69fe568379657943a036fd5f1006bcd1af2f3
MrJaroslavik <MrJaroslavik@gmail.com>
2026-05-05 15:07:07 +0200

    Update frec-data.js

--- schedules/frec-data.js
+++ schedules/frec-data.js
@@ -3,93 +3,100 @@
 
 export const FREC_WEEKENDS = [
 
-{
+  {
     slug: "frec_spielberg_2026",
     name: "Spielberg",
+    link: "https://fiafrec.com/race/spielberg/",
     sessions: [
-      { slug: "qualifying", label: "Qualifying 1", datetime: "2026-04-24 17:20" },
-      { slug: "race1", label: "Race", datetime: "2026-04-25 10:25" },
-      { slug: "race2", label: "Race 2", datetime: "2026-04-25 16:40" },
+      { slug: "qualifying1", label: "Qualifying 1", datetime: "2026-04-24 17:20" },
+      { slug: "race1", label: "Race 1", datetime: "2026-04-25 10:25" },
       { slug: "qualifying2", label: "Qualifying 2", datetime: "2026-04-26 08:45" },
-      { slug: "race3", label: "Race 3", datetime: "2026-04-26 12:55" },
+      { slug: "race2", label: "Race 2", datetime: "2026-04-26 12:55" },
     ],
-},
+  },
 
-{
-slug: "frec_zandvoort_2026",
-name: "Zandvoort",
-sessions: [
-{ slug: "qualifying1", label: "Qualifying 1", datetime: "2026-05-23" },
-{ slug: "race1", label: "Race 1", datetime: "2026-05-23 17:45" },
-{ slug: "qualifying2", label: "Qualifying 2", datetime: "2026-05-24" },
-{ slug: "race2", label: "Race 2", datetime: "2026-05-24 13:30" },
-],
-},
+  {
+    slug: "frec_zandvoort_2026",
+    name: "Zandvoort",
+    link: "https://fiafrec.com/race/zandvoort/",
+    sessions: [
+      { slug: "qualifying1", label: "Qualifying 1", datetime: "2026-05-22" },
+      { slug: "race1", label: "Race 1", datetime: "2026-05-23 17:45" },
+      { slug: "qualifying2", label: "Qualifying 2", datetime: "2026-05-24" },
+      { slug: "race2", label: "Race 2", datetime: "2026-05-24 13:30" },
+    ],
+  },
 
-{
-slug: "frec_spa_2026",
-name: "Spa-Francorchamps",
-sessions: [
-{ slug: "qualifying1", label: "Qualifying 1", datetime: "2026-05-30" },
-{ slug: "race1", label: "Race 1", datetime: "2026-05-30 14:20" },
-{ slug: "qualifying2", label: "Qualifying 2", datetime: "2026-05-31" },
-{ slug: "race2", label: "Race 2", datetime: "2026-05-31 16:15" },
-],
-},
+  {
+    slug: "frec_spa_2026",
+    name: "Spa-Francorchamps",
+    link: "https://fiafrec.com/race/spa-francorchamps/",
+    sessions: [
+      { slug: "qualifying1", label: "Qualifying 1", datetime: "2026-05-29" },
+      { slug: "race1", label: "Race 1", datetime: "2026-05-30 14:20" },
+      { slug: "qualifying2", label: "Qualifying 2", datetime: "2026-05-31" },
+      { slug: "race2", label: "Race 2", datetime: "2026-05-31 16:15" },
+    ],
+  },
 
-{
-slug: "frec_monza_2026",
-name: "Monza",
-sessions: [
-{ slug: "qualifying1", label: "Qualifying 1", datetime: "2026-06-20" },
-{ slug: "race1", label: "Race 1", datetime: "2026-06-20 12:50" },
-{ slug: "qualifying2", label: "Qualifying 2", datetime: "2026-06-21" },
-{ slug: "race2", label: "Race 2", datetime: "2026-06-21 15:50" },
-],
-},
+  {
+    slug: "frec_monza_2026",
+    name: "Monza",
+    link: "https://fiafrec.com/race/monza/",
+    sessions: [
+      { slug: "qualifying1", label: "Qualifying 1", datetime: "2026-06-19" },
+      { slug: "race1", label: "Race 1", datetime: "2026-06-20 12:50" },
+      { slug: "qualifying2", label: "Qualifying 2", datetime: "2026-06-21" },
+      { slug: "race2", label: "Race 2", datetime: "2026-06-21 15:50" },
+    ],
+  },
 
-{
-slug: "frec_hungaroring_2026",
-name: "Hungaroring",
-sessions: [
-{ slug: "qualifying1", label: "Qualifying 1", datetime: "2026-07-04" },
-{ slug: "race1", label: "Race 1", datetime: "2026-07-04 13:30" },
-{ slug: "qualifying2", label: "Qualifying 2", datetime: "2026-07-05" },
-{ slug: "race2", label: "Race 2", datetime: "2026-07-05 15:40" },
-],
-},
+  {
+    slug: "frec_hungaroring_2026",
+    name: "Hungaroring",
+    link: "https://fiafrec.com/race/hungaroring/",
+    sessions: [
+      { slug: "qualifying1", label: "Qualifying 1", datetime: "2026-07-03" },
+      { slug: "race1", label: "Race 1", datetime: "2026-07-04 13:30" },
+      { slug: "qualifying2", label: "Qualifying 2", datetime: "2026-07-05" },
+      { slug: "race2", label: "Race 2", datetime: "2026-07-05 15:40" },
+    ],
+  },
 
-{
-slug: "frec_paulricard_2026",
-name: "Paul Ricard",
-sessions: [
-{ slug: "qualifying1", label: "Qualifying 1", datetime: "2026-07-18" },
-{ slug: "race1", label: "Race 1", datetime: "2026-07-18 13:00" },
-{ slug: "qualifying2", label: "Qualifying 2", datetime: "2026-07-19" },
-{ slug: "race2", label: "Race 2", datetime: "2026-07-19 12:30" },
-],
-},
+  {
+    slug: "frec_paulricard_2026",
+    name: "Paul Ricard",
+    link: "https://fiafrec.com/race/paul-ricard/",
+    sessions: [
+      { slug: "qualifying1", label: "Qualifying 1", datetime: "2026-07-17" },
+      { slug: "race1", label: "Race 1", datetime: "2026-07-18 13:00" },
+      { slug: "qualifying2", label: "Qualifying 2", datetime: "2026-07-19" },
+      { slug: "race2", label: "Race 2", datetime: "2026-07-19 12:30" },
+    ],
+  },
 
-{
-slug: "frec_imola_2026",
-name: "Imola",
-sessions: [
-{ slug: "qualifying1", label: "Qualifying 1", datetime: "2026-09-05" },
-{ slug: "race1", label: "Race 1", datetime: "2026-09-05 16:40" },
-{ slug: "qualifying2", label: "Qualifying 2", datetime: "2026-09-06" },
-{ slug: "race2", label: "Race 2", datetime: "2026-09-06 13:50" },
-],
-},
+  {
+    slug: "frec_imola_2026",
+    name: "Imola",
+    link: "https://fiafrec.com/race/imola/",
+    sessions: [
+      { slug: "qualifying1", label: "Qualifying 1", datetime: "2026-09-04" },
+      { slug: "race1", label: "Race 1", datetime: "2026-09-05 16:40" },
+      { slug: "qualifying2", label: "Qualifying 2", datetime: "2026-09-06" },
+      { slug: "race2", label: "Race 2", datetime: "2026-09-06 13:50" },
+    ],
+  },
 
-{
-slug: "frec_hockenheim_2026",
-name: "Hockenheim",
-sessions: [
-{ slug: "qualifying1", label: "Qualifying 1", datetime: "2026-09-12" },
-{ slug: "race1", label: "Race 1", datetime: "2026-09-12 16:30" },
-{ slug: "qualifying2", label: "Qualifying 2", datetime: "2026-09-13" },
-{ slug: "race2", label: "Race 2", datetime: "2026-09-13 10:15" },
-],
-},
+  {
+    slug: "frec_hockenheim_2026",
+    name: "Hockenheim",
+    link: "https://fiafrec.com/race/hockenheim/",
+    sessions: [
+      { slug: "qualifying1", label: "Qualifying 1", datetime: "2026-09-11" },
+      { slug: "race1", label: "Race 1", datetime: "2026-09-12 16:30" },
+      { slug: "qualifying2", label: "Qualifying 2", datetime: "2026-09-13" },
+      { slug: "race2", label: "Race 2", datetime: "2026-09-13 10:15" },
+    ],
+  },
 
 ];
